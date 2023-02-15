d524f242358d4d45a5dde9c889e53a4d9010637c
sewakgautam <sewak.gautam58@gmail.com>
2023-02-15 12:08:02 +0545

    🎉 init(class validator):
    added class validator on dto

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateRoomDto {
+  @IsString()
   @ApiProperty({ type: 'string', format: 'binary' })
   user: string;
 
+  @IsString()
   @ApiProperty({ type: 'string', format: 'binary' })
   room: string;
 }
