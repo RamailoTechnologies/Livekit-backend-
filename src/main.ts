0ec1be4484683f848d94fcb6262ae279bff9e296
sewakgautam <sewak.gautam58@gmail.com>
2023-02-15 12:15:55 +0545

    cors resolved

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      allowedHeaders: '*',
+    },
+  });
   const config = new DocumentBuilder()
     .setTitle('Live Video And Audio Call')
     .setDescription('meet / Skype Clone system')
